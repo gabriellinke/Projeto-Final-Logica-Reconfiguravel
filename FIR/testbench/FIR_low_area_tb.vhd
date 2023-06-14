75f94f1caf26e4d70c3a9c845c8d474d50df5e27
jgmsilva <jgui.silva1510@gmail.com>
2023-06-14 10:22:47 -0300

    tb escrevendo .dat

--- FIR/testbench/FIR_low_area_tb.vhd
+++ FIR/testbench/FIR_low_area_tb.vhd
@@ -18,6 +18,7 @@ USE ieee.std_logic_1164.all;
 USE ieee.std_logic_arith.all;
 USE ieee.std_logic_signed.all;
 USE ieee.math_real.all;     
+use std.textio.all;
 
 LIBRARY work;
 USE work.fir_package.all;
@@ -31,84 +32,51 @@ ARCHITECTURE FIR_low_area_tb_arch OF FIR_low_area_tb IS
  
 -- TESTCASE SETTINGS
 --.............................................................................
-  CONSTANT freq_xn     : REAL        := 900.0e3;   -- input frequency
+  CONSTANT freq_xn     : REAL        := 2.0e6;   -- input frequency
   --CONSTANT freq_xn     : REAL        := 900.0e3;   -- input frequency
   
   CONSTANT data_length : NATURAL     := 8;        -- input bit size
                                     
   CONSTANT fs_Hz       : REAL        := 8.0e6;     -- sampling frequency
   --CONSTANT fs_Hz       : REAL        := 44.1e3;     -- sampling frequency
-  CONSTANT taps        : NATURAL     := 65;        -- order+1
+  CONSTANT taps        : NATURAL     := 49;        -- order+1
   -- transition band: 800e3 to 1.6e6
   CONSTANT hn 			  : COEFF_ARRAY :=(                     -- normalized coefficients bi: (bo,b1, ..., bN). They must be symmetric (but sign)
-													-0.0001595245,
-													-0.0003084001,
-													-0.0004301811,
-													-0.0004347041,
-													-0.0002055598,
-													0.0003800131,
-													0.0014207502,
-													0.0029545219,
-													0.0049219064,
-													0.0071389552,
-													0.0092882381,
-													0.0109355139,
-													0.0115755740,
-													0.0107053911,
-													0.0079165629,
-													0.0029933899,
-													-0.0040009533,
-													-0.0126686824,
-													-0.0222680859,
-													-0.0317605256,
-													-0.0399147787,
-													-0.0454594627,
-													-0.0472644494,
-													-0.0445247222,
-													-0.0369167302,
-													-0.0246989636,
-													-0.0087353363,
-													0.0095688399,
-													0.0284147387,
-													0.0458241845,
-													0.0598907824,
-													0.0690314079,
-													0.0722001024,
-													0.0690314079,
-													0.0598907824,
-													0.0458241845,
-													0.0284147387,
-													0.0095688399,
-													-0.0087353363,
-													-0.0246989636,
-													-0.0369167302,
-													-0.0445247222,
-													-0.0472644494,
-													-0.0454594627,
-													-0.0399147787,
-													-0.0317605256,
-													-0.0222680859,
-													-0.0126686824,
-													-0.0040009533,
-													0.0029933899,
-													0.0079165629,
-													0.0107053911,
-													0.0115755740,
-													0.0109355139,
-													0.0092882381,
-													0.0071389552,
-													0.0049219064,
-													0.0029545219,
-													0.0014207502,
-													0.0003800131,
-													-0.0002055598,
-													-0.0004347041,
-													-0.0004301811,
-													-0.0003084001,
-													-0.0001595245,
+													0.0001941042,
+                                       0.0006868266,
+                                       0.0015712643,
+                                       0.0031632958,
+                                       0.0057532538,
+                                       0.0095608923,
+                                       0.0146958011,
+                                       0.0211287766,
+                                       0.0286784589,
+                                       0.0370157426,
+                                       0.0456862828,
+                                       0.0541491260,
+                                       0.0618274047,
+                                       0.0681654164,
+                                       0.0726854808,
+                                       0.0750378728,
+                                       0.0750378728,
+                                       0.0726854808,
+                                       0.0681654164,
+                                       0.0618274047,
+                                       0.0541491260,
+                                       0.0456862828,
+                                       0.0370157426,
+                                       0.0286784589,
+                                       0.0211287766,
+                                       0.0146958011,
+                                       0.0095608923,
+                                       0.0057532538,
+                                       0.0031632958,
+                                       0.0015712643,
+                                       0.0006868266,
+                                       0.0001941042,
 													OTHERS=>0.0);         -- (always end with "others=>0.0")
 													
-  CONSTANT bits_resol  : NATURAL := 128;
+  CONSTANT bits_resol  : NATURAL := 32;
 --..............................................................................
 
   SIGNAL areset        : STD_LOGIC;
@@ -125,7 +93,10 @@ ARCHITECTURE FIR_low_area_tb_arch OF FIR_low_area_tb IS
   CONSTANT num_cycles  : NATURAL := 16;
   CONSTANT fs_period   : TIME    := integer(1.0e12/fs_Hz)* 1 ps;
   CONSTANT num_samples : NATURAL := num_cycles*integer(round(fs_Hz/freq_xn));
-  CONSTANT amplitude   : REAL    := 2.0**real(data_length-1)-1.0; 
+  CONSTANT amplitude   : REAL    := 2.0**real(data_length-1)-1.0;
+ 
+  file fptr: text;
+  CONSTANT FILE_NAME :string := "output.dat";
  
 BEGIN
 
@@ -165,6 +136,7 @@ BEGIN
     yn       => yn_unsigned
     );
     
+
   sampling_clock: process 
   --.............................................
   begin
@@ -180,11 +152,14 @@ BEGIN
     variable rand        : real;
     variable noise_n     : real;
     variable sinus_n     : real;
+    variable fstatus  :file_open_status;
+    variable file_line :line;
   begin 
     UNIFORM(seed1, seed2, rand);     -- uniform  0.0 to 1.0
     noise_n := 2.0*(rand-0.5) * (amplitude/20.0);     
     sinus_n := amplitude * sin(2.0*PI*radians) + noise_n;
-    
+    file_open(fstatus, fptr, FILE_NAME, write_mode);
+
     -- saturation control
     if sinus_n > amplitude then
       sinus_n := amplitude;
@@ -194,7 +169,8 @@ BEGIN
     
     xn_signed   <= std_logic_vector(conv_signed(integer(round(sinus_n)),data_length));
     xn_unsigned <= std_logic_vector(conv_unsigned(integer(round(amplitude + sinus_n)),data_length));
-    
+    write(file_line, sinus_n, left, 2);
+    writeline(fptr, file_line);
     if radians >= 1.0 then
       radians <= 0.0;
     else
